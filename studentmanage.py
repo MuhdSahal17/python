ad4a119d99e2a780247b4b161171cf50911d7a65
MuhdSahal <muhdsahalkb@gmail.com>
2025-12-11 19:03:28 +0530

    Update studentmanage.py

--- studentmanage.py
+++ studentmanage.py
@@ -1,13 +1,24 @@
+
+#count students
+
 count = int(input("Enter number of students:"))
+
+# create empty list
+
 marks = []
 
 for i in range(count):
+    
+    #creat a new dict in a list
+    
     student = {}
     student["name"] = input("Enter Student Name:")
     student["maths"] = int(input("Enter maths:"))
     student["science"] = int(input("Enter science:"))
     student["social"] = int(input("Enter social:"))
     marks.append(student)
+
+#defines functions
 
 total = lambda a,b,c: a+b+c
 
@@ -17,16 +28,23 @@ def avg(a,b,c):
     
 print("\n----- RESULTS -----")
 
+
+#iterates the marks
+
 for student in marks:
     a = student["maths"]
     b = student["science"]
     c = student["social"]
-
+    
     print(student["name"])
+
+    #function call
+    
     print("Average=",avg(a,b,c))
     print("Total=",total(a,b,c))
 
-
+    #Grade
+    
     if avg(a,b,c) >= 90:
         print("A Grade")
     elif avg(a,b,c) >= 80:
@@ -38,4 +56,5 @@ for student in marks:
     elif avg(a,b,c) >= 50:
         print("E Grade")
     else:
+
         print("Failed")
